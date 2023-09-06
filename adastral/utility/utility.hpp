0608c01ee6b9800382a96b1dad10c724b912edbc
kate <kate@dariox.club>
2023-09-06 16:13:35 +0800

    Add methods to extract tar/zstd

--- adastral/utility/utility.hpp
+++ adastral/utility/utility.hpp
@@ -1,12 +1,20 @@
 #pragma once
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <filesystem>
+
+#include <zstd.h>
+#include <archive.h>
+#include <archive_entry.h>
+
 
 class Utility
 {
     public:
     static int ExecWithParam(const std::vector<std::string>& params);
     static int DeleteDirectoryContent(const std::filesystem::path& dir);
+    static int ExtractZStd(const std::string& szInputFile, const std::string& szOutputDirectory);
+    static int ExtractTar(const std::string& szInputFile, const std::filesystem::path& szOutputDirectory);
 };
