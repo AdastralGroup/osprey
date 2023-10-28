6784a83e30c454a1409e26e99842915a9333d694
ROBERTS_PC <rob5300@hotmail.com>
2023-10-28 19:23:46 +0100

    migrate over steam sourcemod get function

--- Code/moss/moss.cpp
+++ Code/moss/moss.cpp
@@ -112,3 +112,40 @@ int moss::sanity_checks() {
   curl_data = "";
   return 0;
 }
+
+#if _WIN32
+inline bool Is64BitWindows()
+{
+#if _WIN64
+    return true;
+#else
+    USHORT ProcessMachine;
+    USHORT NativeMachine;
+    BOOL IsWow64 = IsWow64Process2(GetCurrentProcess(), &ProcessMachine, &NativeMachine);
+
+    if(IsWow64)
+    {
+        if(NativeMachine == IMAGE_FILE_MACHINE_AMD64) return true;
+    }
+
+    return false;
+#endif
+}
+#endif
+
+static std::filesystem::path moss::GetSteamSourcemodPath()
+{
+#if _WIN32
+	char valueData[MAX_PATH];
+	DWORD valueLen = MAX_PATH;
+
+	//Check if this is 64 bit or 32 bit process
+	const char* subKey = Is64BitWindows() ? "SOFTWARE\\WOW6432Node\\Valve\\Steam" : "\\SOFTWARE\\Valve\\Steam";
+	RegGetValueA(HKEY_LOCAL_MACHINE, subKey, "InstallPath", RRF_RT_ANY, nullptr, &valueData, &valueLen);
+	return std::filesystem::path(valueData) / "steamapps\\sourcemods";
+#else
+	//Return normal steam path or use sym link version
+	auto normal_steam_linux =  std::filesystem::path("~/.local/share/Steam/steamapps/sourcemods");
+	return std::filesystem::exists(normal_steam_linux) ? normal_steam_linux : std::filesystem::path("~/.steam/steam/steamapps/sourcemods");
+#endif
+}
--- Code/moss/moss.hpp
+++ Code/moss/moss.hpp
@@ -17,6 +17,7 @@ class moss {
   static bool CheckSDKInstalled(const std::filesystem::path& steamDir);
   int sanity_checks();
   std::string get_string_data_from_server(const std::string& url);
+  static std::filesystem::path GetSteamSourcemodPath();
  private:
   static size_t static_curl_callback(void *buffer, size_t sz, size_t n, void *cptr);
   void curl_callback(void *buffer, size_t sz, size_t n); // callback for the below. string only. we could r
