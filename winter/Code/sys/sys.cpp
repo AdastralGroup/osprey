e85a35cff7762784f9ea4abcd55130c0ad99ade6
int-72h <intcoms@kmatter.net>
2024-09-01 23:48:23 +0100

    add Get/SetLaunchArgsForRecentUser

--- winter/Code/sys/sys.cpp
+++ winter/Code/sys/sys.cpp
@@ -40,12 +40,31 @@ int sys::DeleteDirectoryContent(const std::filesystem::path& dir) {
 int sys::SetLaunchArgsForRecentUser(std::filesystem::path steam_path, std::string appid, std::string args) {
   std::filesystem::path localconfig = GetLocalConfigPathForRecentUser(steam_path);
   auto localconfig_vdf = ParseVDFFile(localconfig);
+  auto apps_vdf = localconfig_vdf.childs["Software"]->childs["Valve"]->childs["Steam"]->childs["apps"]->childs[appid];
+  if(apps_vdf->attribs.contains("LaunchOptions")) {
+    apps_vdf->attribs["LaunchOptions"].erase();
+  }
+  apps_vdf->attribs["LaunchOptions"] = args;
+  std::ofstream file(localconfig);
+  if (!file.is_open()) {
+    throw std::runtime_error("invalid file!");
+  }
+  std::ostream objOstream (std::cout.rdbuf());
+  tyti::vdf::write(file,localconfig_vdf);
+  std::flush(file);
+  file.close();
   return 0;
 
 
 }
 std::string sys::GetLaunchArgsForRecentUser(std::filesystem::path steam_path, std::string appid) {
-
+  std::filesystem::path localconfig = GetLocalConfigPathForRecentUser(steam_path);
+  auto localconfig_vdf = ParseVDFFile(localconfig);
+  auto apps_vdf = localconfig_vdf.childs["Software"]->childs["Valve"]->childs["Steam"]->childs["apps"]->childs[appid];
+  if(apps_vdf->attribs.contains("LaunchOptions")) {
+    return apps_vdf->attribs["LaunchOptions"];
+  }
+  return "";
 }
 
 
@@ -54,15 +73,13 @@ std::string sys::GetLocalConfigPathForRecentUser(std::filesystem::path steam_pat
   auto loginusers_vdf = ParseVDFFile(loginusers);
   std::string id;
   for(auto& user : loginusers_vdf.childs) {
-    A_printf("[sys/GetLocalConfigPathForRecentUser] id %s mostrecent %s\n",user.first.c_str(),user.second->attribs["MostRecent"].c_str());
     if(user.second->attribs["MostRecent"] == "1") {
       id = user.first;
     }
   }
   std::string id3 = std::to_string(std::stol(id) & 4294967295); // 2^32-1, we need to turn the id64 into the id3 bottom component so we saw off the top 32 bits
   std::filesystem::path localconfig_path = steam_path / "userdata" / id3 / "config" / "localconfig.vdf";
-  A_printf("[sys/GetLocalConfigPathForRecentUser] Local config path is %s",localconfig_path.c_str());
-  return "beans";
+  return localconfig_path;
 }
 
 
@@ -78,7 +95,9 @@ tyti::vdf::object sys::ParseVDFFile(std::filesystem::path file_path) {
   if (!file.is_open()) {
     throw std::runtime_error("invalid file!");
   }
-  tyti::vdf::object retval = tyti::vdf::read(file);
+  tyti::vdf::Options opts;
+  opts.strip_escape_symbols = false;
+  tyti::vdf::object retval = tyti::vdf::read(file,opts);
   return retval;
 }
 
