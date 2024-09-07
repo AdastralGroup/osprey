ff4eb5304239469e7c228b6de4ef3074261f81f6
intcoms <intcoms@kmatter.net>
2024-09-07 09:42:37 +0100

    Fix launching on Linux (proton) (#29)
    * THIS SHOULD WORK.., launching on linux w/ proton
    
    * fix typo
    
    * refactor: remove redundant proton_map_to_depot
    
    ---------
    
    Co-authored-by: adi ✨ <63885722+fjuton@users.noreply.github.com>

--- winter/Code/palace/palace.cpp
+++ winter/Code/palace/palace.cpp
@@ -251,10 +251,6 @@ std::filesystem::path palace::get_app_path(const std::string &app_id)
 }
 
 #define SOURCE_SDK_2013_APP_ID "243750"
-
-// no idea if there is somewhere an map like that in my steam directory and i don't wanna read every app manifest / call steam api over this silly thing
-const std::map<std::string, std::string> proton_map_to_depot = {{"proton_experimental", "1493710"}};
-
 // returns non-zero if failed
 int palace::launch_game(const std::string &game_name, const std::string &arguments)
 {
@@ -266,33 +262,8 @@ int palace::launch_game(const std::string &game_name, const std::string &argumen
     }
 #ifdef WIN32
     std::string sdk_app_binary = (sdk_app_path / "hl2.exe").string();
-#else
-
-    auto source_sdk_compat_tool = config_file.childs["Software"]->childs["Valve"]->childs["Steam"]->childs["CompatToolMapping"]->childs[SOURCE_SDK_2013_APP_ID];
-    /* "InstallConfigStore"->"Software"->"Valve"->"Steam"->CompatToolMapping */
-    std::string sdk_app_binary;
-    if (source_sdk_compat_tool)
-    {
-        return 1; // proton doesn't work for now.
-        const char *proton_app_name = source_sdk_compat_tool->attribs["name"].c_str();
-        if (!proton_map_to_depot.contains(proton_app_name))
-        {
-            A_error("%s proton version couldn't be found\n", proton_app_name);
-            return 1;
-        }
-        setenv("STEAM_COMPAT_DATA_PATH", (steam_path / "steamapps" / "compatdata").string().c_str(), 1);
-        setenv("STEAM_COMPAT_CLIENT_INSTALL_PATH", steam_path.c_str(), 1);
-        std::filesystem::path proton_app_path = get_app_path(proton_map_to_depot.at(proton_app_name));
-        sdk_app_binary.append("python3 \"" + proton_app_path.string() + "/\"proton waitforexitandrun \"" + (sdk_app_path / "hl2.exe\"").string());
-    }
-    else
-    {
-        sdk_app_binary.append((sdk_app_path / "hl2.sh").string());
-    }
-#endif
     char *command_line = new char[1024]; // yeah i don't think anyone needs more
     snprintf(command_line, 1024, "%s -game sourcetest -steam -game \"%s\" -steam -secure %s", sdk_app_binary.c_str(), (sourcemods_path / game_name).string().c_str(), arguments.c_str());
-#ifdef WIN32
     STARTUPINFO dummy_si = {0};
     dummy_si.cb = sizeof(dummy_si);
     PROCESS_INFORMATION dummy_pi = {0};
@@ -303,8 +274,14 @@ int palace::launch_game(const std::string &game_name, const std::string &argumen
         return 1;
     }
 #else
-    setenv("SteamEnv", "1", 1);
-    if (popen(command_line, "r") == NULL)
+    std::string url_launch = "steam -applaunch ";
+    url_launch += SOURCE_SDK_2013_APP_ID;
+    url_launch += " -game ";
+    url_launch += (sourcemods_path / game_name).string();
+    url_launch += " ";
+    url_launch += arguments;
+    A_printf("%s",url_launch.c_str());
+    if (popen(url_launch.c_str(), "r") == NULL)
     {
         return 1;
     }
