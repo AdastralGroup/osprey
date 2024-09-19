413ed16ca113071bb80124f8974bf5133f90ea6d
Nour-Elhamshary <nour.elhamshary@outlook.com>
2024-09-19 11:23:24 +0300

    Added documentation on public classes and methods of Kachemak

--- winter/Code/emley/kachemak/kachemak.hpp
+++ winter/Code/emley/kachemak/kachemak.hpp
@@ -9,6 +9,7 @@
 #include <version/version.hpp>
 #include <torrent.hpp>
 
+//! Structure for data files that deals with Kachemak.
 struct KachemakVersion
 {
   std::string file_name;
@@ -26,28 +27,108 @@ struct KachemakPatch
   std::size_t temp_required;
 };
 
+//! Enum class that specifies the category of the directory to check if it has free space.
 enum class FreeSpaceCheckCategory
 {
   Temporary,
   Permanent
 };
 
+//! The Kachemak class.
 class Kachemak : public Version
 {
  public:
+    //! Constructor of the class.
+   /*!
+    \param sourcemod_path The path of the sourcemod directory, found within Steam directory.
+    \param folder_name The name of the folder for the game.
+    \param source_url The link to the game's downloadable path online.
+    \param butler_path The path of butler.
+   */
   Kachemak(const std::filesystem::path& sourcemod_path, const std::filesystem::path& folder_name,
            const std::string& source_url, const std::filesystem::path& butler_path);
+
+  //! Function to install sourcemod games.
+  /*!
+    \return Status code for the installation.
+  */
   virtual int install();
+  //! Function to install sourcemod games through a specific path.
+  /*!
+  * \param custom_path Path where the game would be installed.
+    \return Status code post-installation.
+  */
   virtual int install_path(std::filesystem::path custom_path);
+
+  //! Function to check on free spaces at specific directories, depending on its category.
+  /*!
+    \param size Specified Size to check on.
+    \param category The category of the directory.
+  */
   int free_space_check(const uintmax_t size, const FreeSpaceCheckCategory& category);
+  //! Function to check on free spaces at specific directories, depending on its path.
+  /*!
+  * \param size Specified Size to check on.
+  * \param category The path of the directory.
+  * 
+  * Status code post-checking are as such:
+  * 0: success
+  * 1: not enough temp storage
+  * 2: not enough permanent storage
+  * \return Status code post-checking.
+  */
   int free_space_check_path(const uintmax_t size, const std::filesystem::path custom_path);
+  //! Function to update games installed by Adastral
+  /*!
+  * Status code post-update are as such:
+  * 0: success
+  * 1: symlink fail
+  * 2: space check fail
+  * 3: already on latest / verification needed first
+  * 
+  * \return Status code post-update.
+  */
   int update();
+
+  //! Function to verify games installed by Adastral
+  /*!
+  * Status code post-verification are as such:
+  * 0: success
+  * 4: not installed
+  * \return Status code post-verification.
+  */ 
   int verify();
+  //! Function to extract games stored as .zip files.
+  /*!
+  * \param input_file Name of the input file.
+  * \param output_directory Name of the output directory.
+  * \param size Size of the file.
+  * Status code post-extract are as such:
+  * -1: failure
+  * 0: success
+  * 1: not enough free space
+  * \return Status code post-extract.
+  */
   int extract(const std::string& input_file, const std::string& output_directory, const size_t& size);
+  //! Function to extract games stored as .zip files to a custom path.
+  /*!
+   * \param input_file Name of the input file.
+   * \param output_directory Path of the output directory.
+   * \param size Size of the file.
+   * Status code post-extract are as such:
+   * -1: failure
+   * 0: success
+   * 1: not enough free space
+   * \return Status code post-extract.
+   */
   int extract_path(const std::string& input_file, const std::string& output_directory, const size_t& size);
+  //! Function to get the tag of an installed game.
   std::string get_installed_version_tag();
+  //! Function to get the latest version code of an installed game.
   std::string get_latest_version_code();
+  //! Function to get the latest version tag of an installed game.
   std::string get_latest_version_tag();
+  //! Boolean that checks whether verification is forced or not.
   bool force_verify = false;
 
  private:
