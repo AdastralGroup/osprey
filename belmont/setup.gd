f014bd181cecfe26284d5fbe9d2e7668ef6b40a2
int-72h <intcoms@kmatter.net>
2024-10-29 20:34:01 +0000

    add local config file (for path storing) + working setup

--- belmont/setup.gd
+++ belmont/setup.gd
@@ -1,18 +1,18 @@
 extends Control
 
-
+var path = ""
 func _on_button_pressed() -> void:
 	var t = get_tree().create_tween()
-	t.tween_property($Camera2D,"position",Vector2(770,0),1).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	t.tween_property(get_node("/root/root/Camera2D"),"position",Vector2(770,-640),1).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
 
 func _on_default_button_pressed() -> void:
-	pass # Replace with function body.
+	get_node("/root/root").call_deferred("finish_setup")
 
 
 func _on_custom_button_pressed() -> void:
 	$FileDialog.popup()
 
 
-
 func _on_file_dialog_dir_selected(dir: String) -> void:
 	print($FileDialog.current_path)
+	get_node("/root/root").finish_setup()
